825ed90ee374b897fe648fef9a11e61ad5152fd0
newlook2024 <abdulvahob2022@gmail.com>
2024-02-14 21:17:16 +0500

    added contact page for desktop

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 let resMenu = document.querySelector('.responsive-nav')
 let burger = document.querySelector('.burger')
 let yopish = document.querySelector('.close')
+let contact = document.querySelector('#contact-page')
+let ccc = document.querySelector('.ccc')
+let contactBtn = document.querySelector('.contact-us')
 
 burger.addEventListener('click', function(){
     resMenu.style.left = "0"
@@ -10,4 +13,13 @@ burger.addEventListener('click', function(){
 yopish.addEventListener('click', function(){
     resMenu.style.left = "-100%"
     yopish.style.left = "-100%"
+})
+
+contactBtn.addEventListener("click", function(){
+    contact.style.display = 'flex'
+    ccc.style.display = 'flex'
+})
+
+ccc.addEventListener('click', function(){
+    contact.style.display = 'none'
 })
